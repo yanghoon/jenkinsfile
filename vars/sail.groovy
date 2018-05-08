5ebc01e8374b41c0e55d4879ae145df5746d9a8f
yanghoon <ssang276@hanmail.net>
2018-05-08 17:03:15 +0900

    Update sail.groovy

--- vars/sail.groovy
+++ vars/sail.groovy
@@ -1,10 +1,10 @@
-def call(){
+def call(def thiz){
   println "SYSTEM :: run @shared-lib/vars/sail.groovy"
   def workspace = new File('.')
   println "SYSTEM :: ${workspace.absolutePath}"
-  println "SYSTEM :: ${env}"
-  println "SYSTEM :: ${env.WORKSPACE}"
-  println "SYSTEM :: ${WORKSPACE}"
+  println "SYSTEM :: ${thiz.env}"
+  println "SYSTEM :: ${thiz.env.WORKSPACE}"
+  println "SYSTEM :: ${thiz.WORKSPACE}"
   
   //Load prepared Jenkinsfiles
   //- https://jenkins.io/doc/pipeline/examples/#load-from-file
