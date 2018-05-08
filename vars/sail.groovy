386d5548db33f5725e9c23c3fde96be7b76cad8e
yanghoon <ssang276@hanmail.net>
2018-05-08 16:00:02 +0900

    Update sail.groovy

--- vars/sail.groovy
+++ vars/sail.groovy
@@ -1,3 +1,9 @@
 def call(){
   println 'SYSTEM :: run @shared-lib/vars/sail.groovy'
+  
+  //Load prepared Jenkinsfiles
+  //- https://jenkins.io/doc/pipeline/examples/#load-from-file
+  
+  def _default = load('Jenkinsfile.default')
+  _default.start()
 }
