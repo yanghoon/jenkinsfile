2f6ceb82aada49825dbc44b51ffa1288fde6f435
yanghoon <ssang276@hanmail.net>
2018-05-08 17:44:37 +0900

    Update sail.groovy

--- vars/sail.groovy
+++ vars/sail.groovy
@@ -17,9 +17,9 @@ def call(){
     
     //Load a pipline config file(.yaml)
     //- https://github.com/jenkinsci/workflow-remote-loader-plugin
-    println '${env.GIT_REPO_URL}'
-    println '${env.GIT_BRANCH}'
-    println '${scm.userRemoteConfigs}'
+    println "${env.GIT_REPO_URL}"
+    println "${env.GIT_BRANCH}"
+    println "${scm.userRemoteConfigs}"
     
     def _yaml = fileLoader.fromGit('pipeline.yaml', env.GIT_REPO_URL, env.GIT_BRANCH, scm.userRemoteConfigs.credentialsId, '')
     println _yaml
