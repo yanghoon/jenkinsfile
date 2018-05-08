db617ca856f71ce40776594b01577137ee3067ca
yanghoon <ssang276@hanmail.net>
2018-05-08 17:16:46 +0900

    Update sail.groovy

--- vars/sail.groovy
+++ vars/sail.groovy
@@ -4,17 +4,19 @@ def call(def thiz){
   println "SYSTEM :: run @shared-lib/vars/sail.groovy"
   def workspace = new File('.')
   println "SYSTEM :: ${workspace.absolutePath}"
-  println "SYSTEM :: ${thiz.env}"
-  println "SYSTEM :: ${thiz.env.WORKSPACE}"
+  //println "SYSTEM :: ${thiz.env}"
+  //println "SYSTEM :: ${thiz.env.WORKSPACE}"
    
   //Load prepared Jenkinsfiles
   //- https://jenkins.io/doc/pipeline/examples/#load-from-file
   
   //def _default = load("${WORKSPACE}/Jenkinsfile.default")
-  def _default = libraryResource 'Jenkinsfile.default'
-  println _default
+  //def _default = libraryResource 'Jenkinsfile.default'
+  //println _default
   
   node {
     println "SYSTEM :: ${pwd()}"
+    def _default = load("${pwd()}/Jenkinsfile.default")
+    println _default
   }
 }
