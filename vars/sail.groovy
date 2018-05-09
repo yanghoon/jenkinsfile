ff241cdb6a1d5b04132ed8d4ea85f2b1978c9ab9
yanghoon <ssang276@hanmail.net>
2018-05-09 14:39:26 +0900

    Update sail.groovy

--- vars/sail.groovy
+++ vars/sail.groovy
@@ -1,10 +1,9 @@
 import static groovy.io.FileType.FILES
 
 def call(){
-  def workspace = new File('.')
   println """
   SYSTEM :: run @shared-lib/vars/sail.groovy
-  SYSTEM :: ${workspace.absolutePath}
+  SYSTEM :: ${new File('.').absolutePath}
   SYSTEM :: ${GroovySystem.version}
   """
   
@@ -14,15 +13,9 @@ def call(){
   //println _default
   
   node {
-    println "SYSTEM :: ${pwd()}"
-    
-    //Load prepared Jenkinsfiles
-    //- https://jenkins.io/doc/pipeline/examples/#load-from-file
-    load("${pwd()}@libs/sail-lib/Jenkinsfile.default")
-    
-    /*
     //Load a pipline config file(.yaml)
     //- https://github.com/jenkinsci/workflow-remote-loader-plugin
+    /*
     println """
     ${scm.repositories}
     ${scm.userRemoteConfigs.credentialsId}
@@ -34,7 +27,7 @@ def call(){
     println _yaml
     */
     
-    /*
+    println "SYSTEM :: ${pwd()}"
     def _yaml
     fileLoader.withGit scm.userRemoteConfigs[0].url, scm.branches[0].name, scm.userRemoteConfigs.credentialsId[0], '', {
       println "SYSTEM :: ${pwd()}"
@@ -55,6 +48,10 @@ def call(){
       _yaml = _files[0] ? new File(_files[0]).text : null
     }
     println _yaml
-    */
+    env._config = _yaml
+    
+    //Load prepared Jenkinsfiles
+    //- https://jenkins.io/doc/pipeline/examples/#load-from-file
+    load("${pwd()}@libs/sail-lib/Jenkinsfile.default")
   }
 }
