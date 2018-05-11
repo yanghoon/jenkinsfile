c7035f6b4d32ad8ade7a72be9be68b4e0ff82ae0
yanghoon <ssang276@hanmail.net>
2018-05-11 14:34:46 +0900

    Update sail.groovy

--- vars/sail.groovy
+++ vars/sail.groovy
@@ -58,6 +58,6 @@ def call(){
     //Path of shared library resources
     //- https://github.com/jenkinsci/workflow-cps-global-lib-plugin/blob/master/src/main/java/org/jenkinsci/plugins/workflow/libs/LibraryAdder.java#L194
     def pipeline = libraryResource 'Jenkinsfile.default'
-    script(pipeline)
+    evaluate pipeline
   }
 }
