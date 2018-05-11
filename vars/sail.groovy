a594d00fb4e634820ee962bf19ab4cd2f54e1800
yanghoon <ssang276@hanmail.net>
2018-05-11 14:40:54 +0900

    Update sail.groovy

--- vars/sail.groovy
+++ vars/sail.groovy
@@ -12,52 +12,20 @@ def call(){
   //println _default
   
   node {
-    //Load a pipline config file(.yaml)
-    //- https://github.com/jenkinsci/workflow-remote-loader-plugin
-    /*
-    println """
-    ${scm.repositories}
-    ${scm.userRemoteConfigs.credentialsId}
-    ${scm.branches[0].name}
-    ${scm.userRemoteConfigs[0].url}
-    """
-    
-    def _yaml = fileLoader.fromGit('pipeline.yaml', scm.userRemoteConfigs[0].url, scm.branches[0].name, scm.userRemoteConfigs.credentialsId[0], '')
-    println _yaml
-    */
-    
     println "SYSTEM :: ${pwd()}"
-    /*
-    def _yaml
-    fileLoader.withGit scm.userRemoteConfigs[0].url, scm.branches[0].name, scm.userRemoteConfigs.credentialsId[0], '', {
-      println "SYSTEM :: ${pwd()}"
-      
-      //_yaml = new File("${pwd()}", "pipeline.yaml").text
-      // Find list of file name
-      // http://mrhaki.blogspot.kr/2009/11/groovy-goodness-finding-files-with.html
-      def _files = new FileNameFinder().getFileNames("${pwd()}", 'pipeline.yaml pipeline.yml')
-      
-      _files.each {
-        log """\
-        | class: ${it.class}
-        | name : ${it}
-        | ${new File(it).text}"""
-      }
-      
-      _yaml = _files[0] ? new File(_files[0]).text : null
-    }
-    println _yaml
-    env._config = _yaml
-    */
+    
+    //Load a pipline config file(.yaml)
     loadConfig()
     
-    //Load prepared Jenkinsfiles
+    //Load prepared Jenkinsfiles (hard coded path)
     //- https://jenkins.io/doc/pipeline/examples/#load-from-file
     //load("${pwd()}@libs/sail-lib/Jenkinsfile.default")
     
-    //Path of shared library resources
+    //Load prepared Jenkinsfiles (load and run string)
     //- https://github.com/jenkinsci/workflow-cps-global-lib-plugin/blob/master/src/main/java/org/jenkinsci/plugins/workflow/libs/LibraryAdder.java#L194
-    def pipeline = libraryResource 'Jenkinsfile.default'
+    //- https://stackoverflow.com/a/43327284
+    def type = 'default'  //TODO: load standard pipline type from .yaml
+    def pipeline = libraryResource "Jenkinsfile.${type}"
     evaluate pipeline
   }
 }
