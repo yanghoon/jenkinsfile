2688d5f0c0839cced5d5dd54ac205adc6e0524c7
yanghoon <ssang276@hanmail.net>
2018-05-14 18:08:26 +0900

    Update loadConfig.groovy

--- vars/loadConfig.groovy
+++ vars/loadConfig.groovy
@@ -1,5 +1,14 @@
 /* Will executed in node */
 def call() {
+  try (SCMFileSystem fs = SCMFileSystem.of(currentBuild.rawBuild.getParent(), scm)) {
+    if (fs != null) {
+      String script = fs.child('pipeline.yaml').contentAsString();
+      env._config = script;
+    } else {
+      listener.getLogger().println("Lightweight checkout support not available, falling back to full checkout.");
+    }
+  }
+  /*
   fileLoader.withGit scm.userRemoteConfigs[0].url, scm.branches[0].name, scm.userRemoteConfigs.credentialsId[0], '', {
     // Find list of file name
     // http://mrhaki.blogspot.kr/2009/11/groovy-goodness-finding-files-with.html
@@ -9,4 +18,5 @@ def call() {
       
     env._config = _files[0] ? new File(_files[0]).text : null
   }
+  */
 }
