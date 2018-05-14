6d35000129f68b3d93e4d4447921d35937325d21
yanghoon <ssang276@hanmail.net>
2018-05-14 18:25:35 +0900

    Update loadConfig.groovy

--- vars/loadConfig.groovy
+++ vars/loadConfig.groovy
@@ -2,12 +2,19 @@
 import jenkins.scm.api.SCMFileSystem
 
 def call() {
+  println '1111'
   SCMFileSystem fs = SCMFileSystem.of(currentBuild.rawBuild.getParent(), scm)
+  println '2222'
     if (fs != null) {
+      println '3333'
       String script = fs.child('pipeline.yaml').contentAsString();
+      println '4444'
       env._config = script;
+      println '5555'
     } else {
+      println '6666'
       listener.getLogger().println("Lightweight checkout support not available, falling back to full checkout.");
+      println '7777'
     }
   /*
   fileLoader.withGit scm.userRemoteConfigs[0].url, scm.branches[0].name, scm.userRemoteConfigs.credentialsId[0], '', {
