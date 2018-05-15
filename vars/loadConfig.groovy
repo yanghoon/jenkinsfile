215ee0afff3507b52ff5eddd65bb132298a635e0
yanghoon <ssang276@hanmail.net>
2018-05-15 12:46:11 +0900

    Update loadConfig.groovy

--- vars/loadConfig.groovy
+++ vars/loadConfig.groovy
@@ -1,9 +1,13 @@
 /* Will executed in node */
 import jenkins.scm.api.SCMFileSystem
+import hudson.plugins.git.GitSCM
 
 def call(def s) {
   println '1111'
-  s = new hudson.plugins.git.GitSCM('http://169.56.99.56:32336/yanghoon/jenkinsfile-test.git')
+  s = new GitSCM('http://169.56.99.56:32336/yanghoon/jenkinsfile-test.git')
+  println '1111 - 1111'
+  s = [branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '13be602e-61cc-469d-a8e9-b3ca297f929e', url: 'https://github.com/yanghoon/jenkinsfile-test.git']]] as GitSCM
+  println '1111 - 2222'
   SCMFileSystem fs = SCMFileSystem.of(currentBuild.rawBuild.getParent(), s)
   println '2222'
     if (fs != null) {
